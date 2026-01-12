f5f05c68caf794164dc3b980aa7a4c8f2ff86004
Rohan J Hopper <Rohan.Hopper@fii-na.com>
2026-01-12 15:11:29 -0500

    switched to using NVflash for getting GPU info and more: - fixed IP line to just use hostname -i, no awk - added logic to check left/right ports depending on which generation of tester is running - only need to run NVFlash once per card, the results are saved to a variable and processed from there - found the pesky LEFTSN.RSP not found and fixed it(i just forgot the $ sign previously)

--- Fox_app/tester-scripts/scraper-script.sh
+++ Fox_app/tester-scripts/scraper-script.sh
@@ -7,8 +7,8 @@
 
 #path to various files
 TEMPLATE=blank-tester-to-server-format.json
-#CFGPATH=/mnt/nv/mods/test/cfg/  #path for actual tester machine
-CFGPATH=.						#path for Rohan's testing
+CFGPATH=/mnt/nv/mods/test/cfg/  	#path for actual tester machine
+#CFGPATH=.							#path for Rohan's testing
 
 
 #create destination filename and save as variable
@@ -19,29 +19,57 @@ cp $TEMPLATE $NEWFILENAME		#copy blank template to file with the new name
 
 #gather information about fixture, cards, etc. give each one a variable
 FIXTURENAME=$(hostname)
-GENTYPE=$(dmidecode -s system-product-name | sed 's/PW-TOM-B-ATX-G5/Gen 5 B tester/ s/PW-TOM-B-ATX-G3/Gen 3 B tester/')
-RACKNO="Rack-"$(hostname | cut -c 8-9)
-FIXTURESN=$(dmidecode -s system-serial-number)			#give this one a shot also
+GENTYPE=$(dmidecode -s system-product-name | sed 's/PW-TOM-B-ATX-G5/Gen 5 B tester/' | sed 's/PW-TOM-B-ATX-G3/Gen 3 B tester/')
+RACKNO="Rack-"$(hostname | cut -c 8-9)					#this seems pointless as fixture name already contains rack information. I'd like to get rid of it.
+FIXTURESN=$(dmidecode -s system-serial-number)			#easier to read than grepping from serial number in dmdidecodee -t system
 TESTTYPE="Debug-or-Refurbish-or-Sort"                   #TODO: determine what type of test is running
-IPADDR=$(hostname -i | awk '{print $2}')                #works on tester but not Rohan's WSL
+IPADDR=$(hostname -i)              
 MACADDR=$(ifconfig | grep ether | awk '{print $2}')     #should I use ifconfig for IPADDR also?
 CREATOR="tester"
 
 
 echo "getting individual GPU info"
 
-LEFTSN=$(grep "serial_number=" $CFGPATH/uutself.cfg.env | sed 's/serial_number=//')         #if only right side is filled in, this will put right GPU information in left slot side. Need to fix
-LEFTPN=$(grep "part_number" $LEFTSN.RSP | sed 's/part_number=//')
-LEFTLOG=
+if [[ -z $(grep 5 <<< $GENTYPE) ]];then	#this is a gen 3 tester if there isn't a 5
+	LEFTPORT=$(lspci | grep 65 | awk '{ print $1 }')
+	RIGHTPORT=$(lspci | grep b3 | awk '{ print $1 }')
+	if [[ -n $LEFTPORT ]];then	#there is something in the left slot
+		LEFTINFO=$(/mnt/nv/nvflash_mfg -B $LEFTPORT --rdobd | grep "BoardPartNumber\|BoardSerialNumber\|Board699PartNumber")
+		LEFTPN=$(sed -e 's/: /:/g' -e 's/ /\n/g' <<< $LEFTINFO | grep -m 1 BoardPartNumber | awk -F ":" '{ print $2}')
+		LEFtSN=$(sed -e 's/: /:/g' -e 's/ /\n/g' <<< $LEFTINFO | grep -m 1 BoardSerialNumber | awk -F ":" '{ print $2}')
+		LEFT699=$(sed -e 's/: /:/g' -e 's/ /\n/g' <<< $LEFTINFO | grep Board699PartNumber | awk -F ":" '{ print $2}')
+		LEFTLOG=
+		fi
+	if [[ -n $RIGHTPORT ]];then	#there is something in the right slot
+		RIGHTINFO=$(/mnt/nv/nvflash_mfg -B $RIGHTPORT --rdobd | grep "BoardPartNumber\|BoardSerialNumber\|Board699PartNumber")
+		RIGHTPN=$(sed -e 's/: /:/g' -e 's/ /\n/g' <<< $RIGHTINFO | grep -m 1 BoardPartNumber | awk -F ":" '{ print $2}')
+		RIGHTSN=$(sed -e 's/: /:/g' -e 's/ /\n/g' <<< $RIGHTINFO | grep -m 1 BoardSerialNumber | awk -F ":" '{ print $2}')
+		RIGHT699=$(sed -e 's/: /:/g' -e 's/ /\n/g' <<< $RIGHTINFO | grep Board699PartNumber | awk -F ":" '{ print $2}')
+		RIGHTLOG=
+	fi
+else		#if this isn't a Gen3 tester it must be a Gen5
+	LEFTPORT=$(lspci | grep 17 | awk '{ print $1 }')
+	RIGHTPORT=$(lspci | grep 9b | awk '{ print $1 }')
+	if [[ -n $LEFTPORT ]];then	#there is something in the left slot
+		LEFTINFO=$(/mnt/nv/nvflash_mfg -B $LEFTPORT --rdobd | grep "BoardPartNumber\|BoardSerialNumber\|Board699PartNumber")
+		LEFTPN=$(sed -e 's/: /:/g' -e 's/ /\n/g' <<< $LEFTINFO | grep -m 1 BoardPartNumber | awk -F ":" '{ print $2}')
+		LEFTSN=$(sed -e 's/: /:/g' -e 's/ /\n/g' <<< $LEFTINFO | grep -m 1 BoardSerialNumber | awk -F ":" '{ print $2}')
+		LEFT699=$(sed -e 's/: /:/g' -e 's/ /\n/g' <<< $LEFTINFO | grep Board699PartNumber | awk -F ":" '{ print $2}')
+		LEFTLOG=
+	fi
+	if [[ -n $RIGHTPORT ]];then	#there is something in the right slot
+		echo "checking right slot at port: $RIGHTPORT"
+		RIGHTINFO=$(/mnt/nv/nvflash_mfg -B $RIGHTPORT --rdobd | grep "BoardPartNumber\|BoardSerialNumber\|Board699PartNumber")
+		RIGHTPN=$(sed -e 's/: /:/g' -e 's/ /\n/g' <<< $RIGHTINFO | grep -m 1 BoardPartNumber | awk -F ":" '{ print $2}')
+		RIGHTSN=$(sed -e 's/: /:/g' -e 's/ /\n/g' <<< $RIGHTINFO | grep -m 1 BoardSerialNumber | awk -F ":" '{ print $2}')
+		RIGHT699=$(sed -e 's/: /:/g' -e 's/ /\n/g' <<< $RIGHTINFO | grep Board699PartNumber | awk -F ":" '{ print $2}')
+		RIGHTLOG=
+	fi
 
-RIGHTSN=$(grep "serial_number2=" $CFGPATH/uutself.cfg.env | sed 's/serial_number2=//')      #will only put right GPU information in if left slot is occupied.
-RIGHTPN=$(grep "part_number" $RIGHTSN.RSP | sed 's/part_number=//')
-RIGHTLOG=
-
-CURRENTSTATION=$(grep current_stc_name $CFGPATH/LEFTSN.RSP | sed 's/current_stc_name=//')       #this should still work if only one GPU is plugged in, regardless of side
+fi
 
 
-
+CURRENTSTATION=$(grep current_stc_name $CFGPATH$LEFTSN.RSP | sed 's/current_stc_name=//')
 
 echo "beginning replacement"
 #run the sed replacements on the newly created template file, using the variables that were just created for each field
@@ -53,17 +81,18 @@ sed -i "s/test_type.*/test_type\":\"$TESTTYPE\",/" $NEWFILENAME
 sed -i "s/test_station.*/test_station\":\"$CURRENTSTATION\",/" $NEWFILENAME
 sed -i "s/ip_address.*/ip_address\":\"$IPADDR\",/" $NEWFILENAME
 sed -i "s/mac_address.*/mac_address\":\"$MACADDR\",/" $NEWFILENAME
-sed -i "s/creator.*/creator\":\"$CREATOR\"/" $NEWFILENAME
+sed -i "s/creator.*/creator\":\"$CREATOR\",/" $NEWFILENAME
 
-sed -i "s/left-PN.*/left-PN\":\"$LEFTPN\"/" $NEWFILENAME
-sed -i "s/left-SN.*/left-SN\":\"$LEFTSN\"/" $NEWFILENAME
-sed -i "s/left-logpath.*/left-logpath\":\"$LEFTLOG\"/" $NEWFILENAME
+sed -i "s/left-PN.*/left-PN\":\"$LEFTPN\",/" $NEWFILENAME
+echo pn done $LEFTPN
+sed -i "s/left-SN.*/left-SN\":\"$LEFTSN\",/" $NEWFILENAME
+echo SN done $LEFTSN
+sed -i "s/left-logpath.*/left-logpath\":\"$LEFTLOG\",/" $NEWFILENAME
 
-sed -i "s/right-PN.*/right-PN\":\"$RIGHTPN\"/" $NEWFILENAME
-sed -i "s/right-SN.*/right-SN\":\"$RIGHTSN\"/" $NEWFILENAME
+sed -i "s/right-PN.*/right-PN\":\"$RIGHTPN\",/" $NEWFILENAME
+echo pn done $RIGHTPN
+sed -i "s/right-SN.*/right-SN\":\"$RIGHTSN\",/" $NEWFILENAME
+echo SN done $RIGHTSN
 sed -i "s/right-logpath.*/right-logpath\":\"$RIGHTLOG\"/" $NEWFILENAME
 
 echo "done :]"
-
-#test adding a line to see if it shows up on tester
-#looks like it worked :] - tester
